e71c21c749ac3533d45f4cce1a279156be311409
通晓宇宙 <2581011320@qq.com>
2024-04-02 12:31:43 +0800

    冗余的配置项

--- pet-trading/src/main/java/com/example/config/WebMvcConfig.java
+++ pet-trading/src/main/java/com/example/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package com.example.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -18,9 +17,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     }
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/avatar/**")
-                .addResourceLocations("classpath:/resources/avatar/");
-    }
+    // @Override
+    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    //     registry.addResourceHandler("/avatar/**")
+    //             .addResourceLocations("classpath:/resources/avatar/");
+    // }
 }
